f830f512c81916084ce1982912a25e31d72eea5f
phxbinh <phxbinh@gmail.com>
2026-06-26 17:34:00 +0700

    Update docs-rag.ts

--- src/lib/ai/nodes/docs-rag.ts
+++ src/lib/ai/nodes/docs-rag.ts
@@ -4,6 +4,14 @@ export async function docsRag(state: any) {
       title: "Hướng dẫn giặt áo hoodie",
       content: "Giặt ở nhiệt độ dưới 30 độ",
     },
+    {
+      title: "Hướng dẫn giặt áo sợi tre",
+      content: "Giặt ở nhiệt độ dưới 35 độ",
+    },
+    {
+      title: "Hướng dẫn giặt quần jean",
+      content: "Giặt ở nhiệt độ dưới 35 độ, giặt bằng tay với chất giặt có chất tẩy nhẹ",
+    },
   ];
 
   return {
