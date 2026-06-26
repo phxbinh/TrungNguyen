6c2d09ba2610e83791791744976c8d34183d719b
phxbinh <phxbinh@gmail.com>
2026-06-26 15:15:09 +0700

    Create product-detail.ts

---
+++ src/lib/ai/nodes/product-detail.ts
@@ -0,0 +1,11 @@
+export async function productDetail(state: any) {
+  return {
+    ...state,
+    product: {
+      id: 1,
+      name: "Áo hoodie",
+      stock: 10,
+      price: 399000,
+    },
+  };
+}
