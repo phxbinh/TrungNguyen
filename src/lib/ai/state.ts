daba48180f214c6c90b3b8f90a4741ff0a8f8306
phxbinh <phxbinh@gmail.com>
2026-06-26 15:21:54 +0700

    Update state.ts

--- src/lib/ai/state.ts
+++ src/lib/ai/state.ts
@@ -1,8 +1,12 @@
-export type AgentState = {
-  input: string;
-  intent?: string;
-  products?: any[];
-  product?: any;
-  docs?: any[];
-  answer?: string;
-};
+import { Annotation } from "@langchain/langgraph";
+
+export const AgentState = Annotation.Root({
+  input: Annotation<string>,
+  intent: Annotation<string | undefined>,
+  products: Annotation<any[] | undefined>,
+  product: Annotation<any | undefined>,
+  docs: Annotation<any[] | undefined>,
+  answer: Annotation<string | undefined>,
+});
+
+export type AgentStateType = typeof AgentState.State;
