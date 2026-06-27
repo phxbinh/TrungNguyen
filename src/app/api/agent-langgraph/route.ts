7c6d5b61f8976de98580fd1bb097e4d5ce0927f8
phxbinh <phxbinh@gmail.com>
2026-06-27 16:33:55 +0700

    Update route.ts

--- src/app/api/agent-langgraph/route.ts
+++ src/app/api/agent-langgraph/route.ts
@@ -26,35 +26,6 @@ export async function POST(req: Request) {
       streamMode: "values",
     }
   );
-/*
-  const stream = createUIMessageStream({
-    execute: async ({ writer }) => {
-      for await (const chunk of result) {
-        const lastMessage = chunk.messages?.at(-1);
-
-        if (!lastMessage) continue;
-
-        const content =
-          typeof lastMessage.content === "string"
-            ? lastMessage.content
-            : Array.isArray(lastMessage.content)
-            ? lastMessage.content
-                .map((c: any) => c.text ?? "")
-                .join("")
-            : "";
-
-        if (!content) continue;
-
-        writer.write({
-          type: "text-delta",
-          id: lastMessage.id ?? crypto.randomUUID(),
-          delta: content,
-        });
-      }
-    },
-  });
-*/
-
 
 const stream = createUIMessageStream({
   execute: async ({ writer }) => {
