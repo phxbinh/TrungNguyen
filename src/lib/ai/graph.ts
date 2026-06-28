f450cb7b05491413b7f5ce19a6e69f09c0743c35
phxbinh <phxbinh@gmail.com>
2026-06-28 22:04:03 +0700

    Update graph.ts

--- src/lib/ai/graph.ts
+++ src/lib/ai/graph.ts
@@ -1,3 +1,39 @@
+
+/*
+import { StateGraph, END } from "@langchain/langgraph";
+import { AgentState } from "./state";
+
+import { detectIntent } from "./nodes/detect-intent";
+import { productSearch } from "./nodes/product-search";
+import { productDetail } from "./nodes/product-detail";
+import { docsRag } from "./nodes/docs-rag";
+import { generalChat } from "./nodes/general-chat";
+import { synthesize } from "./nodes/synthesize";
+import { extractParams } from "./nodes/extractParams";
+import { routeIntent } from "./router";
+
+export const graph = new StateGraph(AgentState)
+  .addNode("detectIntent", detectIntent)
+  .addNode("productSearch", productSearch)
+  .addNode("productDetail", productDetail)
+  .addNode("docsRag", docsRag)
+  .addNode("generalChat", generalChat)
+  .addNode("synthesize", synthesize)
+
+  .addEdge("__start__", "detectIntent")
+
+  .addConditionalEdges("detectIntent", routeIntent)
+
+  .addEdge("productSearch", "synthesize")
+  .addEdge("productDetail", "synthesize")
+  .addEdge("docsRag", "synthesize")
+  .addEdge("generalChat", "synthesize")
+  .addEdge("synthesize", END)
+
+  .compile();
+*/
+
+
 /*
 import { StateGraph, END } from "@langchain/langgraph";
 import { AgentState } from "./state";
