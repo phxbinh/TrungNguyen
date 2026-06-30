b1da372c46fab8b88d558dd4c4a98feaad181759
phxbinh <phxbinh@gmail.com>
2026-06-30 23:48:27 +0700

    Update product-search.ts

--- src/lib/ai/nodes/product-search.ts
+++ src/lib/ai/nodes/product-search.ts
@@ -1,7 +1,7 @@
 export async function productSearch(state: any) {
   const products = [
-    { id: 1, name: "Áo hoodie" },
-    { id: 2, name: "Áo thun" },
+    { id: 1, name: "Áo hoodie", price: 300000, size: "M"},
+    { id: 2, name: "Áo thun", price: 450000, size: "L" },
   ];
 
   return {
