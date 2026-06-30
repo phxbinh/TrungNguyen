271ab4d9712dd447b82af7faacf27bbd12468f36
phxbinh <phxbinh@gmail.com>
2026-07-01 01:51:31 +0700

    Create graph-agent-child.ts

---
+++ src/lib/ai/graph-agent-child.ts
@@ -0,0 +1,26 @@
+import { StateGraph, END } from "@langchain/langgraph";
+import { AgentState } from "./state";
+
+import { detectIntent } from "./nodes/detect-intent";
+import { productAgentNode } from "./product-agent/product-agent-node";
+import { docsRag } from "./nodes/docs-rag";
+import { generalChat } from "./nodes/general-chat";
+import { synthesize } from "./nodes/synthesize";
+import { routeIntent } from "./router-agent-child";
+
+export const graph = new StateGraph(AgentState)
+  .addNode("detectIntent", detectIntent)
+  .addNode("productAgent", productAgentNode)
+  .addNode("docsRag", docsRag)
+  .addNode("generalChat", generalChat)
+  .addNode("synthesize", synthesize)
+
+  .addEdge("__start__", "detectIntent")
+  .addConditionalEdges("detectIntent", routeIntent)
+
+  .addEdge("productAgent", "synthesize")
+  .addEdge("docsRag", "synthesize")
+  .addEdge("generalChat", "synthesize")
+
+  .addEdge("synthesize", END)
+  .compile();
