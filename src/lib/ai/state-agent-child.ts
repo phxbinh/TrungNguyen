ba62170b8311712904989ad5a24f566735d4ef6c
phxbinh <phxbinh@gmail.com>
2026-07-01 17:23:46 +0700

    Update state-agent-child.ts

--- src/lib/ai/state-agent-child.ts
+++ src/lib/ai/state-agent-child.ts
@@ -1,3 +1,4 @@
+/*
 import { Annotation } from "@langchain/langgraph";
 
 export const AgentState = Annotation.Root({
@@ -17,3 +18,26 @@ export const AgentState = Annotation.Root({
 });
 
 export type AgentStateType = typeof AgentState.State;
+*/
+
+
+import { Annotation } from "@langchain/langgraph";
+
+export const AgentState = Annotation.Root({
+  input: Annotation<string>,
+  intents: Annotation<string[] | undefined>,
+
+  productQuery: Annotation<string | undefined>,
+  docsQuery: Annotation<string | undefined>,
+
+  products: Annotation<any[] | undefined>,
+  product: Annotation<any | undefined>,
+  docs: Annotation<any[] | undefined>,
+
+  answer: Annotation<string | undefined>,
+});
+
+export type AgentStateType = typeof AgentState.State;
+
+
+
