949d5046645b9bc7b498847d1396e44ffb1b1a4d
phxbinh <phxbinh@gmail.com>
2026-07-02 13:43:35 +0700

    Update synthesize.ts

--- src/lib/parent-child-agent/nodes/synthesize.ts
+++ src/lib/parent-child-agent/nodes/synthesize.ts
@@ -1,31 +1,5 @@
 
 
-/*
-import { model } from "../model";
-
-export async function synthesize(state: any) {
-  const res = await model.invoke(`
-User: ${state.input}
-
-Data:
-${JSON.stringify({
-  products: state.products,
-  product: state.product,
-  docs: state.docs,
-  answer: state.answer,
-})}
-
-Generate final answer.
-`);
-
-  return {
-    ...state,
-    answer: String(res.content),
-  };
-}
-*/
-
-
 import { model } from "../model";
 
 export async function synthesize(state: any) {
