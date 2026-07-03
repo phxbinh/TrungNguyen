fcaa806be8f3dfc24d05b226d68a7abfdf36dd23
phxbinh <phxbinh@gmail.com>
2026-07-03 18:05:36 +0700

    Update model.ts

--- src/lib/ai/model.ts
+++ src/lib/ai/model.ts
@@ -2,5 +2,6 @@ import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 
 export const model = new ChatGoogleGenerativeAI({
   model: "gemini-2.5-flash",
-  apiKey: process.env.GOOGLE_GENERATIVE_AI_API_KEY!,
+  //apiKey: process.env.GOOGLE_GENERATIVE_AI_API_KEY!,
+  apiKey: process.env.GOOGLE_GEMINI_KEY_API_LANGCHAIN!,
 });
