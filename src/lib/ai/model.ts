df1cab5def792a5dcd087fa5fef72b8f70118e8f
phxbinh <phxbinh@gmail.com>
2026-07-04 10:51:05 +0700

    Update model.ts

--- src/lib/ai/model.ts
+++ src/lib/ai/model.ts
@@ -1,6 +1,6 @@
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 
-export const model = new ChatGoogleGenerativeAI({
+export const model_ = new ChatGoogleGenerativeAI({
   model: "gemini-3.1-flash-lite",
   //apiKey: process.env.GOOGLE_GENERATIVE_AI_API_KEY!,
   apiKey: process.env.GOOGLE_GEMINI_KEY_API_LANGCHAIN!,
@@ -17,3 +17,17 @@ export const model = new ChatGoogleGenerativeAI({
 
 //gemma-4-31b-it
 //-> Trả kết quà kém
+
+
+export const model = new ChatGoogleGenerativeAI({
+  model: "gemini-3.1-flash-lite",
+  //apiKey: process.env.GOOGLE_GENERATIVE_AI_API_KEY!,
+  apiKey: process.env.GOOGLE_GEMINI_KEY_API_LANGCHAIN!,
+  temperature: 0,
+  thinkingConfig: {
+    thinkingBudget: 0, // <--- ĐÂY LÀ CHÌA KHÓA TIẾT KIỆM TOKEN
+  },
+});
+
+
+
