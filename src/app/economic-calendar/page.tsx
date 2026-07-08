4bf8d01bb6e6a3b106491344cb9b15263f9407c6
phxbinh <phxbinh@gmail.com>
2026-07-08 11:17:50 +0700

    Update page.tsx

--- src/app/economic-calendar/page.tsx
+++ src/app/economic-calendar/page.tsx
@@ -155,6 +155,10 @@ export default function EconomicCalendar() {
           {filteredEvents.length === 0 && !loading && (
             <p className="text-center py-12 text-gray-500">Không có sự kiện nào khớp filter</p>
           )}
+
+<iframe src="https://sslecal2.investing.com?columns=exc_flags,exc_currency,exc_importance,exc_actual,exc_forecast,exc_previous&features=datepicker,timezone&countries=25,32,6,37,72,22,17,39,14,10,35,43,56,36,110,11,26,12,4,5&calType=week&timeZone=8&lang=1" width="650" height="467" frameborder="0" allowtransparency="true" marginwidth="0" marginheight="0"></iframe><div class="poweredBy" style="font-family: Arial, Helvetica, sans-serif;"><span style="font-size: 11px;color: #333333;text-decoration: none;">Real Time Economic Calendar provided by <a href="https://www.investing.com/" rel="nofollow" target="_blank" style="font-size: 11px;color: #06529D; font-weight: bold;" class="underline_link">Investing.com</a>.</span></div>
+
+
         </div>
       </div>
     </div>
